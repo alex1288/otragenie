c2c0fe98667f624d1f0a03bb323c21b8f30cf8ac
alex1288 <alex9301709044@gmail.com>
2018-01-15 14:39:30 +0300

    main

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$recepient = "alex9301709044@gmail.com";
+$sitename = "BestOne";
+
+$name = trim($_GET["name"]);
+$phone = trim($_GET["phone"]);
+
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$message = "Имя: $name \nТелефон: $phone ";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
